c9b896d7c2da2f2d85c75594576a8fe4827aac13
Kyle Shu <shujunyi@gmail.com>
2024-09-05 15:31:02 +0800

    fallback to v4

--- skyplane/api/config.py
+++ skyplane/api/config.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from skyplane import compute
 
-from skyplane.config_paths import aws_quota_path, gcp_quota_path, azure_standardDv5_quota_path, scp_quota_path
+from skyplane.config_paths import aws_quota_path, gcp_quota_path, azure_quota_path, scp_quota_path
 from pathlib import Path
 
 
@@ -97,7 +97,7 @@ class TransferConfig:
     scp_use_spot_instances: bool = False
 
     aws_instance_class: str = "m5.8xlarge"
-    azure_instance_class: str = "Standard_D2_v5"
+    azure_instance_class: str = "Standard_D2_v4"
     gcp_instance_class: str = "n2-standard-16"
     ibmcloud_instance_class: str = "bx2-2x8"
     gcp_use_premium_network: bool = True
@@ -106,7 +106,7 @@ class TransferConfig:
 
     aws_vcpu_file: Path = aws_quota_path
     gcp_vcpu_file: Path = gcp_quota_path
-    azure_vcpu_file: Path = azure_standardDv5_quota_path
+    azure_vcpu_file: Path = azure_quota_path
     # Add SCP Support
     scp_vcpu_file: Path = scp_quota_path
     # TODO: add ibmcloud when the quota info is available
