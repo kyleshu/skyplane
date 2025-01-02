d6750187e5d2e714c6e1876e0d281dfffa1db531
kyleshu <shujunyi@gmail.com>
2025-01-02 18:22:38 +0800

    Update client.py

--- skyplane/api/client.py
+++ skyplane/api/client.py
@@ -106,13 +106,13 @@ class SkyplaneClient:
     def copy_with_dp(self, src: str, dst: str, dp, recursive: bool = False, num_vms: Optional[int] = 1):
         pipeline = self.pipeline(max_instances=num_vms)
         pipeline.queue_copy(src, dst, recursive=recursive)
-        dp, duration, vm_durations = pipeline.start_no_provision_no_deprovision(dp, progress=False)
+        dp, duration, vm_durations = pipeline.start_no_provision_no_deprovision(dp, progress=True, debug=True)
         return dp, duration, vm_durations
 
     def copy_with_no_deprov(self, src: str, dst: str, recursive: bool = False, num_vms: Optional[int] = 1):
         pipeline = self.pipeline(max_instances=num_vms)
         pipeline.queue_copy(src, dst, recursive=recursive)
-        dp, duration, vm_durations = pipeline.start_no_deprovision(progress=False)
+        dp, duration, vm_durations = pipeline.start_no_deprovision(progress=True, debug=True)
         return dp, duration, vm_durations
 
     def deprovision(self, dp):
